3a93e545b4c00f9ded8e2780d0832d05197b30c1
Sofiane GUEZZAR <sofiane.guezzar@cgi.com>
2023-02-07 14:40:07 +0100

    build: componentContainer fixes

--- src/utils/components/ComponentContainer.tsx
+++ src/utils/components/ComponentContainer.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataType, Property } from 'csstype';
-import { Button, styled } from "@mui/material";
+import { Button, Divider, styled } from "@mui/material";
 import { useEffect, useLayoutEffect, useState } from "react";
+import { transform } from "lodash";
 
 enum Location {
     top = 'top',
@@ -19,7 +20,7 @@ enum Position {
 type PositionType = Position | keyof typeof Position;
 const PositionFlex = {
     [Position.start]: 'flex-start',
-    [Position.end]: 'flex-end', 
+    [Position.end]: 'flex-end',
     [Position.center]: 'center'
 };
 
@@ -35,26 +36,22 @@ const LegendContainer = styled('span', {
     slot: 'Root',
     overridesResolver: (props, styles) => styles.root,
 })<ComponentContainerProps & LegendRootProps>(({ theme, ...props }) => ({
+    backgroundColor: 'white',
     position: "absolute",
     display: 'flex',
     alignItems: props.position ? PositionFlex[props.position] : PositionFlex[Position.center],
     justifyContent: props.position ? PositionFlex[props.position] : PositionFlex[Position.center],
     alignContent: 'center',
-    top: props.location === Location.top ? theme.spacing(-1) : (props.location === Location.left || props.location === Location.right ? theme.spacing(1) : 'auto'),
-    bottom: props.location === Location.bottom ? theme.spacing(-1) : 'auto',
-    left: props.location === Location.left ? theme.spacing(-2) : (props.location === Location.top || props.location === Location.bottom ? theme.spacing(1) : 'auto'),
-    right: props.location === Location.right ? theme.spacing(-2) : 'auto',
-    width: props.location === Location.top || props.location === Location.bottom ? 'calc(100% - 2 * ' + theme.spacing(Number(props.padding)) + ')' : 'auto',
-    height: props.location === Location.left || props.location === Location.right ? 'calc(100% - 2 * ' + theme.spacing(Number(props.padding)) + ')' : 'auto',
-}));
-
-const LegendPart = styled('span', {
-    name: 'LegendPart',
-    slot: 'Root',
-    overridesResolver: (props, styles) => styles.root,
-})<{}>(({ theme, ...props }) => ({
-    backgroundColor: 'white',
-    padding: "0 10px 0 10px"
+    top: props.location === Location.top ? '-1px' : 'auto',
+    bottom: props.location === Location.bottom ? '-1px' : 'auto',
+    left: props.location === Location.left ? '-1px' : (props.location === Location.top || props.location === Location.bottom ? theme.spacing(theme.shape.borderRadius / 2) : 'auto'),
+    right: props.location === Location.right ? '-1px' : 'auto',
+    width: props.location === Location.top || props.location === Location.bottom ? 'calc(100% - ' + theme.spacing(theme.shape.borderRadius) + ')' : 'auto',
+    height: props.location === Location.left || props.location === Location.right ? 'calc(100% - ' + theme.spacing(theme.shape.borderRadius) + ')' : 'auto',
+    ...(props.location === Location.top && { transform: 'translateY(-50%)' }),
+    ...(props.location === Location.bottom && { transform: 'translateY(50%)' }),
+    ...(props.location === Location.left && { transform: 'translateX(-50%)' }),
+    ...(props.location === Location.right && { transform: 'translateX(50%)' }),
 }));
 
 type TLength = (string & {}) | 0;
@@ -112,14 +109,14 @@ function ComponentContainer(props: ComponentContainerProps) {
             width='fit-content'
             sx={{
                 p: props.padding ?? 0,
-                borderStyle: props.borderStyle,
-                borderWidth: props.borderWidth,
-                borderColor: props.borderColor,
-                borderRadius: props.borderRadius,
-                paddingTop: topComponentHeight + 'px',
-                paddingBottom: bottomComponentHeight + 'px',
-                paddingLeft: leftComponentWidth + 'px',
-                paddingRight: rightComponentWidth + 'px',
+                borderStyle: props.borderStyle ?? 'solid',
+                borderWidth: props.borderWidth ?? '1px',
+                borderColor: (theme) => props.borderColor ?? theme.palette.divider,
+                borderRadius: (theme) => props.borderRadius ?? theme.shape.borderRadius,
+                paddingTop: topComponentHeight/2 + 'px',
+                paddingBottom: bottomComponentHeight/2 + 'px',
+                paddingLeft: leftComponentWidth/2 + 'px',
+                paddingRight: rightComponentWidth/2 + 'px',
             }}
         >
             {props.Legend &&
@@ -130,9 +127,10 @@ function ComponentContainer(props: ComponentContainerProps) {
                             ref={refTop}
                             location='top'
                             position={props.Legend.top.position}
-                            padding={props.padding}
                         >
-                            <LegendPart>{props.Legend.top.component}</LegendPart>
+                            <Box component='span' width='100%'>
+                                <Divider>{props.Legend.top.component}</Divider>
+                            </Box>
                         </LegendContainer>
                     }
                     {
@@ -141,9 +139,10 @@ function ComponentContainer(props: ComponentContainerProps) {
                             ref={refBottom}
                             location='bottom'
                             position={props.Legend.bottom.position}
-                            padding={props.padding}
                         >
-                            <LegendPart>{props.Legend.bottom.component}</LegendPart>
+                            <Box component='span' width='100%'>
+                                <Divider>{props.Legend.bottom.component}</Divider>
+                            </Box>
                         </LegendContainer>
                     }
                     {
@@ -152,9 +151,10 @@ function ComponentContainer(props: ComponentContainerProps) {
                             ref={refLeft}
                             location='left'
                             position={props.Legend.left.position}
-                            padding={props.padding}
                         >
-                            <LegendPart>{props.Legend.left.component}</LegendPart>
+                            <Box component='span' height='100%'>
+                                <Divider orientation='vertical'>{props.Legend.left.component}</Divider>
+                            </Box>
                         </LegendContainer>
                     }
                     {
@@ -163,9 +163,10 @@ function ComponentContainer(props: ComponentContainerProps) {
                             ref={refRight}
                             location='right'
                             position={props.Legend.right.position}
-                            padding={props.padding}
                         >
-                            <LegendPart>{props.Legend.right.component}</LegendPart>
+                            <Box component='span' height='100%'>
+                                <Divider orientation='vertical'>{props.Legend.right.component}</Divider>
+                            </Box>
                         </LegendContainer>
                     }
                 </>
@@ -180,15 +181,15 @@ export default ComponentContainer;
 // export default function BoxComponent() {
 //     return (
 //         <ComponentContainer
-//             borderStyle="solid"
-//             borderWidth="2px"
-//             borderColor="lightgray"
+//             // borderStyle="solid"
+//             // borderWidth="2px"
+//             // borderColor="lightgray"
 //             padding={1}
-//             borderRadius='10px'
+//             // borderRadius='10px'
 //             Legend={{
 //                 top: { component: <>rty</> },
-//                 bottom: { component: <Button variant="contained">Save</Button> },
-//                 left: { component: <>Left</>, position: 'end' },
+//                 bottom: { component: <Button variant="contained">Save</Button>, position: 'start' },
+//                 left: { component: <Button variant="contained" sx={{ height: '200%' }}>Save</Button>, position: 'end' },
 //                 right: { component: <>right</> },
 //             }}
 //         >
