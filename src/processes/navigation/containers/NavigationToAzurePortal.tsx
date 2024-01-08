ce36f47471355c64a64e858a05c0ff26799f059f
Sofiane GUEZZAR <sofiane.guezzar@cgi.com>
2024-01-08 17:23:40 +0100

    navigation: Azure Portal button

---
+++ src/processes/navigation/containers/NavigationToAzurePortal.tsx
@@ -0,0 +1,46 @@
+
+import { Button, Tooltip } from '@mui/material';
+import { Stack } from '@mui/system';
+import React from 'react';
+
+// import NavigationIcon from '@mui/icons-material/Navigation';
+
+import ComponentContainer from '../../../utils/components/ComponentContainer';
+import { PowerAppsIcon, PowerEnvironmentIcon } from '../icons';
+import { NavigationButton } from '../../../utils/types/NavigationButton';
+
+function Environments(props: NavigationButton) {
+    const { environmentId, clientUrl } = props;
+
+    function handleClick() {
+        window.open(`https://portal.azure.com/#home`, '_blank');
+    }
+    return (
+        <>
+            {/* <ComponentContainer width='100%' Legends={{ top: { position: 'center', component: 'Environments', padding: '5px' } }}>
+                <Stack spacing={1} width='calc(100% - 10px)' padding='5px' direction='row'> */}
+            <Tooltip placement='left' title='Azure Portal'>
+                <Button
+                    variant='outlined'
+                    onClick={handleClick}
+                    startIcon={<PowerEnvironmentIcon />}
+                    sx={{
+                        width: '100%',
+                        maxWidth: 'calc(100% - 10px)',
+                        gap: '0.4em',
+                        padding: '5px 10px',
+                        justifyContent:'flex-start',
+                        overflow:'hidden',
+                        whiteSpace:'nowrap',
+                    }}
+                >
+                    Azure Portal
+                </Button>
+            </Tooltip>
+            {/* </Stack>
+            </ComponentContainer> */}
+        </>
+    )
+}
+
+export default Environments;
