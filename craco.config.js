787f8c070b915c9968b0e05f52b32349f9ee3c8d
Sofiane <sofiane.guezzar@cgi.com>
2025-03-07 14:55:47 +0100

    Remove config files

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,8 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+// const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const smp = new SpeedMeasurePlugin()
+// const smp = new SpeedMeasurePlugin()
 
 module.exports = {
     webpack: {
